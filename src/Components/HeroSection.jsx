17a5fcecc1f1c4bb8bfec16ee4cea90915fb898e
jihan212 <jihanbjashim@gmail.com>
2023-03-02 22:45:54 +0600

    Hero section completed

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import hero from '../images/hero.png';
+import heroCorner from '../images/heroCorner.png';
 
 const HeroSection = () => {
-  return (
-    <div>HeroSection</div>
-  )
-}
+	return (
+		<div className='HeroSection'>
+			<div className='heroText'>
+				<img
+					src={heroCorner}
+					alt=''
+				/>
+				<div>
+					<h1>Freshly Brewed Coffee</h1>
+					<p>
+						Everyday in Arabika is like the colourful seasons of
+						Bangladesh. Arabika welcomes all its customers to come
+						and create special memories.
+					</p>
+				</div>
+			</div>
+			<div className='heroImage'>
+				<img
+					src={hero}
+					alt=''
+				/>
+			</div>
+		</div>
+	);
+};
 
-export default HeroSection
+export default HeroSection;
--- src/Components/page/Homepage.jsx
+++ src/Components/page/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../Navbar';
+import HeroSection from '../HeroSection';
 
 const Homepage = () => {
 	return (
 		<>
 			<Navbar />
+			<HeroSection />
 		</>
 	);
 };
